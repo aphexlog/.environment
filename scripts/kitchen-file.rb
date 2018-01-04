4c15d109edb3512b75f5c14188d0dd1981491ca7
Aaron West <aaron.west@accenture.com>
2018-01-04 17:11:16 -0600

    YOLO

--- scripts/kitchen-file.rb
+++ scripts/kitchen-file.rb
@@ -1,9 +1,10 @@
 # This ruby script allows the user to execute test kitchen against any .kitchen file within the current working directory.
 
-puts "What kitchen.yml file should we use?"
+puts "What .kitchen.yml file should we use?"
 kitchen_yml = gets.chomp
-puts "Using #{ kitchen_yml }"
+puts "Using .kitchen-#{ kitchen_yml }.yml"
 puts "What kitchen command are we using?"
 kitchen_cmd = gets.chomp
 puts "Using #{ kitchen_cmd }"
-exec "KITCHEN_YAML=\"#{ kitchen_yml }\" #{ kitchen_cmd }"
+puts "Executing the following: KITCHEN_YAML=\".kitchen-#{ kitchen_yml }.yml\" kitchen #{ kitchen_cmd }"
+exec "KITCHEN_YAML=\".kitchen-#{ kitchen_yml }.yml\" kitchen #{ kitchen_cmd }"
